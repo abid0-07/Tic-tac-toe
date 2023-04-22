2f9ffa3e6031a6eb65bad74eecb88098e9f79879
abid0-07 <abid35-757@diu.edu.bd>
2023-04-22 20:04:47 +0600

    Added scripts files

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+function openPlayerConfig(){
+    playerConfigOverlyElement.style.display = "block";
+    backdropElement.style.display = "block";
+}
+
+function closePlayerConfig(){
+    playerConfigOverlyElement.style.display = "none";
+    backdropElement.style.display = "none";
+}
